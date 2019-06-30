3da8e2fe5e6feed87a22afb4d20032a8f8bedc47
Alken <50940365+adisc0@users.noreply.github.com>
2019-06-30 11:57:52 +0800

    Add files via upload

---
+++ Cars/TestCar.java
@@ -0,0 +1,12 @@
+public class TestCar {
+
+    public static void main(String[] args) {
+
+        Japanese j = new Japanese("Toyota","Black");
+        American a = new American("Ford","White");
+
+        a.getDetails();
+        j.getDetails();
+    }
+
+}
